77afc0385d2977f208d3b87ac108cbf3df7acdf4
neoinsanity <raulg@talksum.com>
2012-10-13 10:06:09 -0700

    Fixing a misplaced line.

--- windmills/lib/__init__.py
+++ windmills/lib/__init__.py
@@ -9,4 +9,4 @@ for module in os.listdir(os.path.dirname(__file__)):
     if module == '__init__.py' or module[-3:] != '.py':
         continue
     __import__(module[:-3], locals(), globals())
-del module
+    del module
