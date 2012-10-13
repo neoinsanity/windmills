293714ec7572fdfcd747f3a9c078fa9243fea655
neoinsanity <raulg@talksum.com>
2012-10-13 09:29:24 -0700

    Added initial setup.py file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='windmill',
+    version='0.0.1',
+    packages=[''],
+    package_dir={'': 'windmills'},
+    url='https://github.com/neoinsanity/windmills',
+    license='Apache License 2.0',
+    author='neoinsanity',
+    author_email='mindbender@gmail.com',
+    description='This is a set of tools and libraries focused on creating 0mq solutions.'
+)
