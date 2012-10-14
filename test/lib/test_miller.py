9fb1816945cadd28a43f456db59235d92c167937
neoinsanity <raulg@talksum.com>
2012-10-13 20:33:18 -0700

    Started the test package for unit test coverage. Still a lot of thinking to be done here. But it's a start.

---
+++ test/lib/test_miller.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+import doctest
+import unittest
+from windmills.lib import Miller
+
+
+__author__ = 'neoinsanity'
+
+class TestMiller(unittest.TestCase):
+
+    def test_default_behavior(self):
+        """
+        Create a default Miller, and exercise it.
+        """
+        miller = Miller()
+        assert miller
