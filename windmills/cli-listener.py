77026a9f2b11a197f04fdc7a455c0b55f63fa4fc
Brett van Zuiden <brettcvz@gmail.com>
2012-10-17 15:57:48 -0700

    Switched over cli to use default

--- windmills/cli-listener.py
+++ windmills/cli-listener.py
@@ -63,6 +63,9 @@ class CliListener(Scaffold):
         #todo: raul - find a better way to do socket.recv handling registring
         # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         # self._input_recv_handler is defined in the Cornerstone class.
+        #Override with out own listener
+        args.recv = '_listener_recv_handler'
+
         #self._input_recv_handler = self._listener_recv_handler
         # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
