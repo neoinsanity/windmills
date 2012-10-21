144a22452f6277d072476858573a598566802d44
neoinsanity <raulg@talksum.com>
2012-10-21 02:00:13 -0700

    Split the docttest from the unittest execution. This made the coverage reporting cleaner. Aded first echo_service unit test.

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,3 @@
 find . -type f -iname \*.pyc -delete
 nosetests -c nose.cfg
+nosetests -c nose-doctest.cfg
--- test/test_echo_service.py
+++ test/test_echo_service.py
@@ -1,7 +1,7 @@
 import os
 from test.utils_of_test import thread_wrap_windmill
 from test.windmill_test_case import WindmillTestCase
-from zmq import Context
+from zmq import Context, REQ
 
 
 __author__ = 'neoinsanity'
@@ -10,6 +10,8 @@ __author__ = 'neoinsanity'
 class TestEchoService(WindmillTestCase):
     def setUp(self):
         self.zmq_ctx = Context()
+        self.req_out_sock = self.zmq_ctx.socket(REQ)
+        self.req_out_sock.bind('tcp://*:8889')
 
         d = 'test_out'
         if not os.path.exists(d):
@@ -17,9 +19,21 @@ class TestEchoService(WindmillTestCase):
 
 
     def tearDown(self):
-        pass
+        self.req_out_sock.close()
 
 
     def test_echo_service_default_behavior(self):
         t = thread_wrap_windmill('EchoService')
+        try:
+            t.start()
+            self.assertTrue(t.is_alive,
+                            'The EchoService instance shouldj have started.')
+            self.req_out_sock.send("echo, echo, echo")
+            msg = self.req_out_sock.recv()
 
+            self.assertEqual("echo, echo, echo", msg)
+        finally:
+            t.windmill.kill()
+            t.join(3)
+            self.assertFalse(t.is_alive(),
+                             'The EchoService instance should have shutdown.')
