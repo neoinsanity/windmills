d4306c558674b5c545c7c49f1da732840906f6e0
neoinsanity <raulg@talksum.com>
2012-11-05 17:04:19 -0800

    Completed the test_echo_service coverage.

--- test/test_echo_service.py
+++ test/test_echo_service.py
@@ -10,8 +10,6 @@ __author__ = 'neoinsanity'
 class TestEchoService(WindmillTestCase):
     def setUp(self):
         self.zmq_ctx = Context()
-        self.req_out_sock = self.zmq_ctx.socket(REQ)
-        self.req_out_sock.bind('tcp://*:8889')
 
         d = 'test_out'
         if not os.path.exists(d):
@@ -19,17 +17,21 @@ class TestEchoService(WindmillTestCase):
 
 
     def tearDown(self):
-        self.req_out_sock.close()
+        pass
 
 
     def test_echo_service_default_behavior(self):
+        req_out_sock = self.zmq_ctx.socket(REQ)
+        req_out_sock.bind('tcp://*:8889')
+
         t = thread_wrap_windmill('EchoService')
+
         try:
             t.start()
             self.assertTrue(t.is_alive,
-                            'The EchoService instance shouldj have started.')
-            self.req_out_sock.send("echo, echo, echo")
-            msg = self.req_out_sock.recv()
+                            'The EchoService instance should have started.')
+            req_out_sock.send("echo, echo, echo")
+            msg = req_out_sock.recv()
 
             self.assertEqual("echo, echo, echo", msg)
         finally:
@@ -37,3 +39,28 @@ class TestEchoService(WindmillTestCase):
             t.join(3)
             self.assertFalse(t.is_alive(),
                              'The EchoService instance should have shutdown.')
+
+            req_out_sock.close()
+
+
+    def test_echo_service_options(self):
+        req_out_sock = self.zmq_ctx.socket(REQ)
+        req_out_sock.bind('tcp://*:8899')
+
+        argv = ['-m', 'pong', '--reply_sock_url', 'tcp://localhost:8899']
+        t = thread_wrap_windmill('EchoService', argv=argv)
+        try:
+            t.start()
+            self.assertTrue(t.is_alive(),
+                            'The EchoService instance should have started.')
+            req_out_sock.send('ping')
+            msg = req_out_sock.recv()
+
+            self.assertEqual('pong', msg)
+        finally:
+            t.windmill.kill()
+            t.join(3)
+            self.assertFalse(t.is_alive(),
+                             'The EchoService instance shold have shutdown.')
+
+            req_out_sock.close()
