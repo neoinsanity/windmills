efab81d59ad128baf9cfa6940d54c8523920ff51
neoinsanity <raulg@talksum.com>
2012-11-08 12:13:45 -0800

    Added the email windmill to the windmills project.

--- windmills/__init__.py
+++ windmills/__init__.py
@@ -1,11 +1,12 @@
 __author__ = 'neoinsanity'
-__all__ = ['CliEmitter', 'CliListener', 'DonQuixote', 'EchoService',
+__all__ = ['CliEmitter', 'CliListener', 'DonQuixote', 'EmailWindmill' 'EchoService',
            'ProxyWindmill', 'RouterDealerWindmill', 'VentilatorWindmill']
 
 from cli_emitter import CliEmitter
 from cli_listener import CliListener
 from don_quixote import DonQuixote
 from echo_service import EchoService
+from email_windmill import EmailWindmill
 from proxy_windmill import ProxyWindmill
 from router_dealer_windmil import RouterDealerWindmill
 from ventilator_windmill import VentilatorWindmill
