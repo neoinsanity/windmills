0fd7eee06af139d325af8813a166fc25a466b059
raulg <raulg@google.com>
2014-01-09 10:08:11 -0800

    Got the input text for new messages done. Doesn't actually send messages, just the UI plumbing done.

--- windmills/web_service/static/js/web-emitter-app.js
+++ windmills/web_service/static/js/web-emitter-app.js
@@ -1,9 +1,30 @@
-
 window.WebEmitter = Ember.Application.create();
 
 WebEmitter.Store = DS.Store.extend({
-   revision: 13,
+    revision: 13,
     adapter: DS.FixtureAdapter
 });
 
-// WebEmitter.ApplicationAdaptor = DS.FixtureAdapter.extend();
+WebEmitter.MessagesController = Ember.ArrayController.extend({
+    actions: {
+        createMessage: function () {
+            // get the message body set by the "New Message" text field
+            var body = this.get('newMessage');
+            if (!body.trim()) {
+                return;
+            }
+
+            // create the new Message model
+            var message = this.store.createRecord('message', {
+                body: body,
+                isSent: false,
+            });
+
+            // clear the "New Message" text field
+            this.set('newMessage', '');
+
+            // save the new modal
+            message.save();
+        }
+    }
+});
