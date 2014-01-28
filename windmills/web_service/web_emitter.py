ede2d2ef802f83ddbe8121baeeb394ce2053d419
raulg <raulg@google.com>
2014-01-27 21:33:24 -0800

    Added REST call to deliver messages.

--- windmills/web_service/web_emitter.py
+++ windmills/web_service/web_emitter.py
@@ -3,20 +3,41 @@ import sys
 from gevent.pywsgi import WSGIServer
 from pyramid.config import Configurator
 from pyramid.response import Response
-from pyramid.view import view_config
 
 from windmills.core import Crate, Shaft
 
 __author__ = 'Raul Gonzalez'
 
 
+def app_ctx_enabled(contexts=list()):
+    def func_handler(func):
+        def func_wrapper(request):
+            appctx = request.appctx()
+            log = appctx.log
+
+            # Retrieve any additional contexts
+            args = list()
+            for context in contexts:
+                args.append(getattr(appctx, context))
+
+            return func(request, appctx, log, *args)
+
+        return func_wrapper
+
+    return func_handler
+
+
 class WebEmitter(Shaft):
     def __init__(self, **kwargs):
-        Shaft.__init__(self, **kwargs)
-        self.cargo = self.declare_cargo()
-
         # wsgi server
         self._wsgi_server = None
+
+        print 'pre configure shaft'
+        Shaft.__init__(self, **kwargs)
+        print 'post configure shaft'
+
+        self.cargo = self.declare_cargo()
+
 
     def appctx(self):
         return self
@@ -26,6 +47,9 @@ class WebEmitter(Shaft):
 
     def configure(self, args=list()):
         assert args
+
+        # Override the jinja2 template default comment and variable tags
+        # so that is doesn't conflict with the Handlebar tags.
         config = Configurator(settings={
             'jinja2.comment_start_string': '(#',
             'jinja2.comment_end_string': '#)',
@@ -48,11 +72,14 @@ class WebEmitter(Shaft):
         config.add_view(self.hello_world, route_name='hello')
         config.add_route('root_url', '/')
         config.add_view(self.root_url, route_name='root_url', renderer='web_emitter.jinja2')
+        config.add_route('send_msg', '/send_msg')
+        config.add_view(self.send_msg, route_name='send_msg', request_method='POST',
+                        renderer='json', accept='application/json')
 
         # Generate the app and start the server.
         app = config.make_wsgi_app()
         self._wsgi_server = WSGIServer(('', 8000), app)
-        self._wsgi_server.serve_forever()  # todo: raul - need to make this non blocking
+        self._wsgi_server.start()  # todo: raul - need to make this non blocking
 
     def run_loop(self):
         self.log.info('... Entering run loop ...')
@@ -65,6 +92,16 @@ class WebEmitter(Shaft):
     def hello_world(request):
         request.appctx().log.debug('request: %s', request)
         return Response('Hello World')
+
+    @staticmethod
+    @app_ctx_enabled(['cargo'])
+    def send_msg(request, appctx, log, cargo):
+        log.debug('request.body: %s', request.body)
+
+        crate = Crate(msg_data=request.body)
+        cargo.send(crate)
+
+        return Response(status=200)
 
 
 if __name__ == '__main__':
