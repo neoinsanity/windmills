d3f80604d651452fc4e02c02e4b957611cf2e1c8
Andu Nguyen <62072885+andu1c@users.noreply.github.com>
2021-06-14 09:08:06 -0700

    Cleanup the lightning server code

--- prototype_lightning/lightning_server.py
+++ prototype_lightning/lightning_server.py
@@ -3,22 +3,13 @@
 #
 from gevent import monkey, spawn
 
-from prototype_lightning.get_distance import get_distance_with_geopandas, get_distance_with_geopy
+from prototype_lightning.get_distance import get_distance_with_geopy
 
 monkey.patch_all()
 
 
 import zmq.green as zmq
 import json
-import logging
-
-formatter = logging.Formatter('%(asctime)s -%(name)s - %(levelname)s -- %(message)s')
-log = logging.getLogger()
-log.setLevel(logging.INFO)
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-console_handler.setFormatter(formatter)
-log.addHandler(console_handler)
 
 airports = [
     {"id": 1, "name": "San Jose Airport", "lat": 37.36384485275424, "lon": -121.92911989199551},
@@ -33,8 +24,6 @@ logger_sender_socket.connect("ipc:///tmp/log.pipe")
 socket = context.socket(zmq.PULL)
 socket.bind("ipc:///tmp/test.pipe")
 
-
-# resi_buildings = []
 
 while True:
     #  Wait for next request from client
